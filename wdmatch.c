5681a317e6617674e5cb8f21c10b1bf0992d40fc
Keandra Bintcliffe <kbintcli@c3r7s9.wethinkcode.co.za>
2020-01-21 18:50:57 +0200

    wdmatch

--- wdmatch.c
+++ wdmatch.c
@@ -6,7 +6,7 @@
 /*   By: kbintcli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/21 10:20:47 by kbintcli          #+#    #+#             */
-/*   Updated: 2020/01/21 18:17:53 by kbintcli         ###   ########.fr       */
+/*   Updated: 2020/01/21 18:50:01 by kbintcli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,31 @@ int		main(int argc, char **argv)
 	int k;
 
 	i = 0;
-	k = 1;
-	while (argv[1][i] != '\0' && argv[2][i] != '\0')
-	{
-		if (argv[1][i] = argv[2][])
-		{
-
-		}
-		write(1, "\n", 1);		
-	}
-	if (argc >= 2)
+	k = 0;
+	if (argc != 3)
 	{
 		write(1, "\n", 1);
+		return (0);
 	}
-	return ();
+	while (argv[1][i] != '\0' && argv[2][k] != '\0')
+	{
+		if (argv[1][i] == argv[2][k])
+		{
+			i++;	
+		}
+		k++;
+	}
+	if (argv[1][i] == '\0')
+	{
+		i = 0;
+		while (argv[1][i] != '\0')
+		{
+			write(1, &argv[1][i], 1);
+			i++;
+		}
+		write(1, "\n", 1);
+	}
+	else
+		write(1, "\n", 1);
+	return (0);
 }
